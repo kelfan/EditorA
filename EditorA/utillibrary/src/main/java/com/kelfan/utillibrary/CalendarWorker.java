5fc46ce6b8a4c83e5396254e19c6d5d448c4f6b9
kelfan <zchaofan08@gmail.com>
2018-07-01 12:49:04 +1000

    datepicker

--- EditorA/utillibrary/src/main/java/com/kelfan/utillibrary/CalendarWorker.java
+++ EditorA/utillibrary/src/main/java/com/kelfan/utillibrary/CalendarWorker.java
@@ -1,15 +1,17 @@
 package com.kelfan.utillibrary;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 
 public class CalendarWorker {
     public static void setDate(final EditText editText, Context context) {
-        final Calendar c = Calendar.getInstance();
+        Calendar c = Calendar.getInstance();
         int day = c.get(Calendar.DAY_OF_MONTH);
         int month = c.get(Calendar.MONTH);
         int year = c.get(Calendar.YEAR);
@@ -17,9 +19,27 @@ public class CalendarWorker {
         DatePickerDialog datePickerDialog = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
-                editText.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                String text = dayOfMonth + "-" + (monthOfYear + 1) + "-" + year;
+                editText.setText(text);
+                editText.setSelection(text.length());
             }
         }, year, month, day);
         datePickerDialog.show();
+    }
+
+    public static void setTime(final EditText editText, Context context) {
+        Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minutes = c.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(context, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                String text = hourOfDay + ":" + minutes;
+                editText.setText(text);
+                editText.setSelection(text.length());
+            }
+        }, hour, minutes, false);
+        timePickerDialog.show();
     }
 }
